aad6f12fbfe7d2c163f2b66b1de0bf846bd7ab5d
grevlinger <155060026+grevlinger@users.noreply.github.com>
2023-12-28 14:08:44 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,22 @@
 let score = 0;
 
+// Hent lagret poengsum ved oppstart
+if (localStorage.getItem('score')) {
+    score = parseInt(localStorage.getItem('score'));
+    updateScore();
+}
+
 function incrementScore() {
     score++;
     updateScore();
+    saveScore();
 }
 
 function updateScore() {
     document.getElementById('score').innerText = `Score: ${score}`;
+}
+
+function saveScore() {
+    // Lagre poengsummen i lokal lagring
+    localStorage.setItem('score', score.toString());
 }
