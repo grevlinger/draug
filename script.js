37cd864a69ee271a70acad7c144a9fd0146cbdf9
grevlinger <155060026+grevlinger@users.noreply.github.com>
2023-12-28 16:07:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// JavaScript game logic goes here
+// You'll need to handle user interactions, game state, etc.
+// For a complex game, consider using a framework like Phaser.js.
 
+document.addEventListener("DOMContentLoaded", function () {
+    // Your initialization code here
+});
