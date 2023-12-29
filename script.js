ea7c388d8f774fbd3fe84679d41b33e772bea1ca
grevlinger <155060026+grevlinger@users.noreply.github.com>
2023-12-29 14:53:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-// JavaScript game logic goes here
-// You'll need to handle user interactions, game state, etc.
-// For a complex game, consider using a framework like Phaser.js.
+// Game variables
+let score = 0;
+let clickValue = 1;
+let idleRate = 1;
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Your initialization code here
-});
+// DOM elements
+const scoreElement = document.getElementById('score');
+const clickButton = document.getElementById('click-button');
+const idleButton = document.getElementById('idle-button');
+
+// Event listeners
+clickButton.addEventListener('click', click);
+idleButton.addEventListener('click', startIdle);
+
+// Click event handler
+function click() {
+    score += clickValue;
+    updateScore();
+}
+
+// Idle event handler
+function startIdle() {
+    setInterval(idle, 1000); // 1000 milliseconds = 1 second
+}
+
+// Idle logic
+function idle() {
+    score += idleRate;
+    updateScore();
+}
+
+// Update score on the UI
+function updateScore() {
+    scoreElement.textContent = `Score: ${score}`;
+}
