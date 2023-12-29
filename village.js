88f532ceda6bc0dad00b08bcc6c68134da0dd09a
grevlinger <155060026+grevlinger@users.noreply.github.com>
2023-12-29 18:13:52 +0100

    Update village.js

--- village.js
+++ village.js
@@ -1,157 +1,67 @@
 let population = parseInt(localStorage.getItem('population')) || 0;
-let resources = parseInt(localStorage.getItem('resources')) || 0;
-let buildingLevel = parseInt(localStorage.getItem('buildingLevel')) || 1;
-let houseCounter = parseInt(localStorage.getItem('houseCounter')) || 0;
-let gold = parseInt(localStorage.getItem('gold')) || 0;
-let upgradeCost = 10; // Initial cost to upgrade house income
-let upgradeMultiplier = 2; // Multiplier for increasing upgrade cost
+let houseCount = parseInt(localStorage.getItem('houseCount')) || 0;
+let villageCount = parseInt(localStorage.getItem('villageCount')) || 0;
 
 const populationElement = document.getElementById('population');
 const houseCountElement = document.getElementById('house-count');
-const goldCountElement = document.getElementById('gold-count');
+const villageCountElement = document.getElementById('village-count');
+const addResidentButton = document.getElementById('add-resident');
+const collectResourcesButton = document.getElementById('collect-resources');
 const upgradeBuildingButton = document.getElementById('upgrade-building');
+const restartGameButton = document.getElementById('restart-game');
 
-document.getElementById('add-resident').addEventListener('click', addResident);
-document.getElementById('collect-resources').addEventListener('click', collectResources);
+addResidentButton.addEventListener('click', addResident);
+collectResourcesButton.addEventListener('click', collectResources);
 upgradeBuildingButton.addEventListener('click', upgradeBuilding);
-document.getElementById('restart-game').addEventListener('click', restartGame);
+restartGameButton.addEventListener('click', restartGame);
 
 updatePopulation();
 updateHouseCount();
-updateGold();
-updateResources();
-createResidentIcons();
+updateVillageCount();
 
-// Add resident event handler
 function addResident() {
     population++;
     updatePopulation();
-    createResidentIcon();
-    checkForHouseTransformation();
-}
-
-// Collect resources event handler
-function collectResources() {
-    resources += population * buildingLevel;
-    updateResources();
-}
-
-// Upgrade building event handler
-function upgradeBuilding() {
-    if (resources >= upgradeCost) {
-        resources -= upgradeCost;
-        upgradeHouse();
-        updateResources();
-    } else {
-        alert("Ikke nok ressurser for å oppgradere husleieinntektene.");
+    if (population % 10 === 0) {
+        // Every tenth resident generates a house
+        houseCount++;
+        updateHouseCount();
+        if (houseCount % 10 === 0) {
+            // Every tenth house forms a village
+            villageCount++;
+            updateVillageCount();
+        }
     }
 }
 
-// Restart game event handler
-function restartGame() {
-    // Reset all game data
-    population = 0;
-    resources = 0;
-    buildingLevel = 1;
-    houseCounter = 0;
-    gold = 0;
-    upgradeCost = 10;
-    // Clear resident icons
-    document.getElementById('resident-icons').innerHTML = '';
-    // Update UI
-    updatePopulation();
-    updateHouseCount();
-    updateGold();
-    updateResources();
+function collectResources() {
+    // Implement resource collection logic based on your design
 }
 
-// Update population on the UI
+function upgradeBuilding() {
+    // Implement building upgrade logic based on your design
+}
+
+function restartGame() {
+    population = 0;
+    houseCount = 0;
+    villageCount = 0;
+    updatePopulation();
+    updateHouseCount();
+    updateVillageCount();
+}
+
 function updatePopulation() {
     populationElement.textContent = `Befolkning: ${population}`;
-    // Save data to local storage
     localStorage.setItem('population', population);
 }
 
-// Update house count on the UI
 function updateHouseCount() {
-    houseCountElement.textContent = `Bolighus: ${houseCounter}`;
-    // Save data to local storage
-    localStorage.setItem('houseCounter', houseCounter);
+    houseCountElement.textContent = `Bolighus: ${houseCount}`;
+    localStorage.setItem('houseCount', houseCount);
 }
 
-// Update gold count on the UI
-function updateGold() {
-    goldCountElement.textContent = `Gull: ${gold}`;
-    // Save data to local storage
-    localStorage.setItem('gold', gold);
-}
-
-// Update resources on the UI
-function updateResources() {
-    // Implement UI update based on your design
-    // Save data to local storage
-    localStorage.setItem('resources', resources);
-    localStorage.setItem('buildingLevel', buildingLevel);
-}
-
-// Create a resident icon
-function createResidentIcon() {
-    const residentIcon = document.createElement('div');
-    residentIcon.className = 'resident-icon';
-    residentIcon.innerHTML = '👨'; // Emoji icon for man
-    document.getElementById('resident-icons').appendChild(residentIcon);
-}
-
-// Create resident icons based on current population
-function createResidentIcons() {
-    for (let i = 0; i < population; i++) {
-        createResidentIcon();
-        checkForHouseTransformation();
-    }
-}
-
-// Check for house transformation
-function checkForHouseTransformation() {
-    if (population >= 10) {
-        // Transform the last 10 residents into a house
-        for (let i = 0; i < 10; i++) {
-            const residentIcon = document.querySelector('.resident-icon');
-            residentIcon.parentNode.removeChild(residentIcon);
-        }
-        createHouseIcon();
-    }
-}
-
-// Create a house icon
-function createHouseIcon() {
-    const houseIcon = document.createElement('div');
-    houseIcon.className = 'house-icon';
-    houseIcon.innerHTML = '🏠'; // Emoji icon for house
-    document.getElementById('resident-icons').appendChild(houseIcon);
-    houseCounter++;
-    updateHouseCount();
-
-    // Start generating passive income from the new house
-    setInterval(generatePassiveIncome, 1000);
-}
-
-// Generate passive income from each house
-function generatePassiveIncome() {
-    gold += houseCounter; // Each house generates 1 gold per second
-    updateGold();
-}
-
-// Upgrade house income
-function upgradeHouse() {
-    houseCounter++; // Increase the income from existing houses
-    upgradeCost = Math.round(upgradeCost * upgradeMultiplier); // Increase the upgrade cost
-    updateHouseCount();
-    updateGold();
-    updateResources();
-    updateUpgradeButton();
-}
-
-// Update upgrade button text
-function updateUpgradeButton() {
-    upgradeBuildingButton.textContent = `Oppgrader Bygning (${upgradeCost} ressurser)`;
+function updateVillageCount() {
+    villageCountElement.textContent = `Landsbyer: ${villageCount}`;
+    localStorage.setItem('villageCount', villageCount);
 }
