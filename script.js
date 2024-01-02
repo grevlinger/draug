5574914f8af5cfa446e9655f99f017cf61eb467d
grevlinger <155060026+grevlinger@users.noreply.github.com>
2024-01-02 11:12:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let gold = 0;
 let goldPerClick = 1;
-let goldPerSecond = 0;
+let houses = 0;
+let passiveIncome = 0;
 let passiveIncomeInterval;
 
 function updateGoldDisplay() {
@@ -17,12 +18,13 @@ function buyUpgrade(upgrade) {
     case 'house':
       if (gold >= 20) {
         gold -= 20;
-        goldPerSecond += 1;
+        houses++;
+        passiveIncome = houses; // Each house adds 1 to passive income
         updateGoldDisplay();
         addEmoji('🏠');
         if (!passiveIncomeInterval) {
-          passiveIncomeInterval = setInterval(function() {
-            gold += goldPerSecond;
+          passiveIncomeInterval = setInterval(function () {
+            gold += passiveIncome;
             updateGoldDisplay();
           }, 1000);
         }
