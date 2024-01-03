c60b03771e5eaf66237b9357d805306de8f25616
grevlinger <155060026+grevlinger@users.noreply.github.com>
2024-01-03 01:12:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let oxygen = 0;
 let co2 = 0;
 let clickValue = 1;
 let plantLevel = 1;
-let upgradeCost = 10;
 let waterCount = 0;
 
 function updateDisplay() {
@@ -10,33 +9,19 @@ function updateDisplay() {
     document.getElementById('co2').textContent = co2;
 }
 
-function clickPlant() {
-    oxygen += clickValue;
-    updateDisplay();
-}
-
-function upgradePlant() {
-    if (oxygen >= upgradeCost) {
-        oxygen -= upgradeCost;
-        clickValue += plantLevel;
-        plantLevel++;
-        upgradeCost = Math.ceil(upgradeCost * 1.5);
-        updateDisplay();
-    }
-}
-
 function waterPlant() {
     waterCount++;
     clickValue += waterCount;
+    co2 -= waterCount;
+    oxygen += Math.ceil(waterCount / 2);
     updateDisplay();
 }
 
 // Automated oxygen and CO2 production every second
 setInterval(function () {
     oxygen += plantLevel;
-    co2 += Math.ceil(plantLevel / 2);
 
-    // Passive CO2 consumption and O2 production
+    // Passive CO2 consumption
     if (waterCount > 0) {
         co2 -= waterCount;
         oxygen += Math.ceil(waterCount / 2);
