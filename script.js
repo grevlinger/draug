750cdc8abf1ff82afb2d10ea4f938e05409e11fa
grevlinger <155060026+grevlinger@users.noreply.github.com>
2024-01-14 00:19:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ function generateGold() {
     if (gold < 20) {
         gold += 1;
         updateGoldText();
+    } else {
+        alert("You have reached the maximum gold limit (20).");
     }
+}
 
 function updateGoldText() {
     document.getElementById("gold").textContent = gold;
