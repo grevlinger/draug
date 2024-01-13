bf5f3b00f238e7562e7abf3d00a8cbfb883d6729
grevlinger <155060026+grevlinger@users.noreply.github.com>
2024-01-13 23:04:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function showInventory() {
 }
 
 function manualLabor() {
-  let goldEarned = manualLaborIncome * (strengthBonus + 0.6); // 60% mer gull med styrkebonus
+  let goldEarned = manualLaborIncome * (strengthBonus * 1.6); // 60% mer gull med styrkebonus
   gold += goldEarned;
   if (gold > walletLimit) {
     gold = walletLimit; // Begrens gullet til lommeboksgrensen
