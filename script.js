8216c59cd97d7ca276c798a5e25452b221fa4e60
grevlinger <155060026+grevlinger@users.noreply.github.com>
2024-01-14 19:00:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
+function chooseClass(characterClass) {
+  var resultContainer = document.getElementById('result');
+  var story;
 
-      gold: 0,
-      goldPerClick: 1,
-      goldPerClickCost: 10,
-      goldPerSecond: 0,
-      goldPerSecondCost: 50,
-      doubleGoldCost: 100
-    };
-
-    // Clear local storage
-    localStorage.removeItem("goldMinerSave");
-
-    // Update UI
-    document.getElementById("goldMined").innerHTML = "0 Gold Mined";
-    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Level 1) Cost: 10 Gold";
-    document.getElementById("perSecondUpgrade").innerHTML = "Upgrade Mining Rig (Level 1) Cost: 50 Gold";
-    document.getElementById("doubleGoldUpgrade").innerHTML = "Double Gold Output Cost: 100 Gold";
-
-    // Restart main game loop and save game loop
-    mainGameLoop = window.setInterval(function () {
-      mineGold();
-    }, 1000);
-
-    saveGameLoop = window.setInterval(function () {
-      localStorage.setItem("goldMinerSave", JSON.stringify(gameData));
-    }, 15000);
+  switch (characterClass) {
+    case 'warrior':
+      story = "You chose the Warrior class. Armed with a mighty sword, you set out on a journey to vanquish evil.";
+      break;
+    case 'mage':
+      story = "You chose the Mage class. Harnessing the power of arcane magic, you embark on a quest to unravel ancient mysteries.";
+      break;
+    case 'rogue':
+      story = "You chose the Rogue class. Master of stealth and agility, you traverse the shadows to achieve your objectives.";
+      break;
+    default:
+      story = "Invalid choice. Please try again.";
+      break;
   }
-}
 
-// ... (rest of the code)
+  resultContainer.innerHTML = `<p>${story}</p>`;
+}
