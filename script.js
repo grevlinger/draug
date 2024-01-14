117006bd58098d821b52b2a793ffef780ebbffea
grevlinger <155060026+grevlinger@users.noreply.github.com>
2024-01-14 19:08:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
-function chooseClass(characterClass) {
-  var resultContainer = document.getElementById('result');
-  var story;
+var storyIndex = 0;
 
-  switch (characterClass) {
-    case 'warrior':
-      story = "You chose the Warrior class. Armed with a mighty sword, you set out on a journey to vanquish evil.";
-      break;
-    case 'mage':
-      story = "You chose the Mage class. Harnessing the power of arcane magic, you embark on a quest to unravel ancient mysteries.";
-      break;
-    case 'rogue':
-      story = "You chose the Rogue class. Master of stealth and agility, you traverse the shadows to achieve your objectives.";
-      break;
-    default:
-      story = "Invalid choice. Please try again.";
-      break;
-  }
-
-  resultContainer.innerHTML = `<p>${story}</p>`;
+function startGame() {
+  showStory();
 }
+
+function showStory() {
+  var storyText = document.getElementById('story-text');
+  var choicesContainer = document.getElementById('choices-container');
+  var resultContainer = document.getElementById('result');
+
+  var currentScene = story[storyIndex];
+
+  storyText.innerHTML = currentScene.text;
+
+  choicesContainer.innerHTML = "";
+  currentScene.choices.forEach(function (choice, index) {
+    var button = document.createElement('button');
+    button.innerHTML = choice.text;
+    button.className = 'choice-button';
+    button.onclick = function () {
+      makeChoice(index);
+    };
+    choicesContainer.appendChild(button);
+  });
+
+  if (currentScene.result) {
+    resultContainer.innerHTML = `<p>${currentScene.result}</p>`;
+  } else {
+    resultContainer.innerHTML = "";
+  }
+}
+
+function makeChoice(choiceIndex) {
+  var currentScene = story[storyIndex];
+  var nextSceneIndex = currentScene.choices[choiceIndex].nextScene;
+  storyIndex = nextSceneIndex;
+  showStory();
+}
+
+var story = [
+  {
+    text: "Welcome to the Adventure Quest! Choose your character class.",
+    choices: [
+      { text: "Warrior", nextScene: 1 },
+      { text: "Mage", nextScene: 2 },
+      { text: "Rogue", nextScene: 3 },
+    ],
+  },
+  {
+    text: "You chose the Warrior class. Armed with a mighty sword, you set out on a journey to vanquish evil.",
+    result: "Your journey begins...",
+  },
+  {
+    text: "You chose the Mage class. Harnessing the power of arcane magic, you embark on a quest to unravel ancient mysteries.",
+    result: "Your magical adventure begins...",
+  },
+  {
+    text: "You chose the Rogue class. Master of stealth and agility, you traverse the shadows to achieve your objectives.",
+    result: "Your stealthy journey begins...",
+  },
+];
+
+startGame();
