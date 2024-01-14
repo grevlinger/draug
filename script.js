70bf1403d5b2f4f6a88acb88308f1a158490fe52
grevlinger <155060026+grevlinger@users.noreply.github.com>
2024-01-14 18:15:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-let points = 0;
-
-function incrementPoints() {
-  points++;
-  updatePointsDisplay();
-}
-
-function buyUpgrade(cost, increase) {
-  if (points >= cost) {
-    points -= cost;
-    incrementPointsByValue(increase);
-    updatePointsDisplay();
-  } else {
-    alert("Not enough points to buy this upgrade!");
-  }
-}
-
-function incrementPointsByValue(value) {
-  points += value;
-}
-
-function updatePointsDisplay() {
-  document.getElementById('points').innerText = points;
+var gameData = {
+  gold: 0,
+  goldPerClick: 1
 }
